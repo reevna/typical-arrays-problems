48cf8d8234c91b39cd215d5a45845f4eea6616f6
github-reevna <shu-@list.ru>
2021-03-13 22:45:02 +0300

    add: solve arrayy problms

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array.length === 0)  return 0;
+ return Math.min.apply(null, array);
+
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === null) return 0;
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === null) return 0;
+  let sum = 0;
+  for (i = 0; i < array.length; i++){
+    sum += array[i];
+  }
+  return sum/array.length;
 }
